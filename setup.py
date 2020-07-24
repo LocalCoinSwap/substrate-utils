a72e10f177016601d9c4dbdfca2095e49c9d6a0a
codeforcer <nathancworsley@gmail.com>
2020-07-24 14:15:55 +0100

    bump scalecodec version

--- setup.py
+++ setup.py
@@ -20,11 +20,11 @@ URL = "https://github.com/LocalCoinSwap/kusama-utils/"
 EMAIL = "nathan@localcoinswap.com"
 AUTHOR = "Nathan Worsley"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 REQUIRED = [
     "websockets==8.1",
-    "scalecodec==0.9.42",
+    "scalecodec==0.10.1",
     "base58==2.0.0",
     "py-sr25519-bindings==0.1.1",
     "xxhash==1.4.3",
