d2bb064ad9bd6e051b73cca346a6e5004ef87c11
codeforcer <nathancworsley@gmail.com>
2020-10-13 10:03:16 +0100

    version bump

--- setup.py
+++ setup.py
@@ -20,11 +20,11 @@ URL = "https://github.com/LocalCoinSwap/kusama-utils/"
 EMAIL = "nathan@localcoinswap.com"
 AUTHOR = "Nathan Worsley"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.1.9"
+VERSION = "0.1.10"
 
 REQUIRED = [
     "websockets==8.1",
-    "scalecodec==0.10.8",
+    "scalecodec==0.10.24",
     "base58==1.0.3",
     "py-sr25519-bindings==0.1.1",
     "xxhash==1.4.3",
