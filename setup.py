d64ed42db0ee84857a3244741aaafc6b568c7a69
Ravi Ojha <ivarojha@gmail.com>
2020-11-18 11:34:47 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = "https://github.com/LocalCoinSwap/substrate-utils/"
 EMAIL = "nathan@localcoinswap.com"
 AUTHOR = "Nathan Worsley"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.2.5"
+VERSION = "0.2.6"
 
 REQUIRED = [
     "websockets==8.1",
