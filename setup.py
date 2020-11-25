e4606007ab0c43b36ba83481dc467225f1ba2ec2
codeforcer <nathancworsley@gmail.com>
2020-11-25 09:44:23 +0000

    version bump 0.2.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = "https://github.com/LocalCoinSwap/substrate-utils/"
 EMAIL = "nathan@localcoinswap.com"
 AUTHOR = "Nathan Worsley"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.2.6"
+VERSION = "0.2.7"
 
 REQUIRED = [
     "websockets==8.1",
