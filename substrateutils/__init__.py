2b1b0cddd27e3c636a200c70fef1d4ce2ea1beb0
Nathan <31618931+CodeForcer@users.noreply.github.com>
2020-11-27 10:30:10 +0000

    Update type registries (#23)
    * function to update type registries
    
    * explanation in requirements
    
    * use patch branch

--- substrateutils/__init__.py
+++ substrateutils/__init__.py
@@ -1,6 +1,7 @@
 from .cores import Kulupu
 from .cores import Kusama
 from .cores import Polkadot
+from .cores import update_registry
 from .network import Network
 
-__all__ = [Kusama, Polkadot, Kulupu, Network]
+__all__ = [Kusama, Polkadot, Kulupu, Network, update_registry]
