c786ec7f78df15083774352d408e24ab2feaa019
T <taylor@localcoinswap.com>
2021-01-13 14:56:26 +1000

    version 0.2.15

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = "https://github.com/LocalCoinSwap/substrate-utils/"
 EMAIL = "nathan@localcoinswap.com"
 AUTHOR = "Nathan Worsley"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.2.14"
+VERSION = "0.2.15"
 
 REQUIRED = [
     "websockets==8.1",
